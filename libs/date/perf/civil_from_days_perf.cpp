30b6ac4be338df3321228ac1f81962f698212589
arunk-s <arunsori94@gmail.com>
2014-06-17 14:03:21 +0530

    Modified civil_from_days_perf test

--- libs/date/perf/civil_from_days_perf.cpp
+++ libs/date/perf/civil_from_days_perf.cpp
@@ -6,6 +6,7 @@
 #include <boost/chrono/date/tuples.hpp>
 #include <boost/chrono/chrono.hpp>
 #include <tuple>
+#include <typeinfo>
 
 using namespace boost::chrono;
 
@@ -37,8 +38,9 @@ void years_civil_from_days(){
       {
 
           auto d_1 = civil_from_days(days_date(m_/day(i)/y).days_since_epoch().count()-dt_1.days_since_epoch().count());
-
-//          BOOST_VERIFY( serial_date==prev+1);
+          BOOST_VERIFY(std::get<0>(d_1) == y);
+          BOOST_VERIFY(std::get<1>(d_1) == m);
+          BOOST_VERIFY(std::get<2>(d_1) == i);
 //          prev = serial_date;
 //          count+=serial_date;
       }
@@ -55,7 +57,45 @@ void years_civil_from_days(){
 
 }
 
+void years_to_ymd(){
+  boost::chrono::days_rep count = 0;
+  int ycount = 0;
+  Clock::time_point t0 = Clock::now();
+  days_date dt_1(jan/day(01)/0001);
+  boost::chrono::days serial_date = dt_1.days_since_epoch() ;//- dt_1.days_since_epoch();
+//  year_month_day ymd = to_ymd(serial_date);
+//  year y(ymd);
+//  month m_
+//  std::cout<<y<<std::endl;
+//  auto d = civil_from_days(serial_date);
+
+  for (int y = Ymin; y <=Ymax; ++y)
+  {
+    for (int m = 1; m <=12; ++m)
+    {
+      month m_ = month(m, no_check);
+      bool is_leap = year(y).is_leap();
+      int day_count = m_.days_in(is_leap).count();
+
+      for(int i = 1 ; i<=day_count ;++i)
+      {
+
+          year_month_day ymd = to_ymd(days_date(m_/day(i)/y).days_since_epoch());
+          BOOST_VERIFY( year(ymd) == y);
+          BOOST_VERIFY(month(ymd) == m_);
+          BOOST_VERIFY(day(ymd) == i);
+//          prev = serial_date;
+//          count+=serial_date;
+      }
+    }
+    ycount++;
+  }
+  Clock::time_point t1 = Clock::now();
+  std::cout << "with to_ymd()                            " << t1-t0<< " Total Years " << ycount << " " <<count << '\n';
+
+}
 int main(){
-	temp();
-	return 1;
+	years_civil_from_days();
+	years_to_ymd();
+  return 1;
 }
