f89180edcb1512b4bdabc299edfe507ea365de88
arunk-s <arunsori94@gmail.com>
2014-06-25 13:55:25 +0530

    Added .gitignore + Merge issues Fixed

--- boost/chrono/date/conversions.hpp
+++ boost/chrono/date/conversions.hpp
@@ -20,13 +20,6 @@ namespace boost
   namespace chrono
   {
 
-<<<<<<< HEAD
-days_rep
-days_from_civil_f(int y, int m, int d) BOOST_NOEXCEPT;
-=======
->>>>>>> f222676280e8ab729b0271ecc1ca6384082d26c1
-
-
     /**
      * days to ymd conversion.
      * @param d the days to convert.
